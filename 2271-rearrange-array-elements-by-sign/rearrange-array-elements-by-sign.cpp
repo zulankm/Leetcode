8142fa4adb214a92538b041c0b66264e29b3623a
Kartikay Mehta <71170646+KartikayMehta@users.noreply.github.com>
2024-02-05 11:48:04 +0530

    Time: 144 ms (84.57%) | Memory: 130.6 MB (36.51%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        int j=0;
+        int k=1;
+        vector<int> modnums(n,0);
+        for(int i=0;i<n;i++)
+        {    
+            if(nums[i]>0)
+            {
+                modnums[j]=nums[i];
+                    j=j+2;
+            
+            }
+            if(nums[i]<0)
+            {
+                modnums[k]=nums[i];
+                    k=k+2;
+            }
+
+        }
+        nums=modnums;
+        return nums;
+    }
+};
