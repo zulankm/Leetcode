1de556a006231d6ce0cd8495ffb6e7e7906977f4
Kartikay Mehta <71170646+KartikayMehta@users.noreply.github.com>
2024-07-16 19:23:50 +0530

    Time: 4 ms (31.14%) | Memory: 10 MB (9.21%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,41 +1,39 @@
+
+      
+       
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        int low=0,mid=0,high=n-1;
-        while(mid<=high)
+             int zero = 0;
+        int one = 0;
+        int two = 0;
+        for(int i = 0;i < nums.size();i++)
         {
-            if(nums[mid]==0)
-            {
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid]==1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(nums[mid],nums[high]);
-                high--;
-            }
+            if(nums[i]==0)zero++;
+            else if(nums[i]==1)one++;
+                else if(nums[i]==2)two++;
         }
-       }
-    
+        for(int i = 0;i < nums.size();i++)
+        {
+            if(zero>0)
+            {
+                nums[i]=0;
+                zero--;
+            }
+            else if(one>0)
+                {
+                    nums[i]=1;
+                    one--;
+                }
+                else if(two>0)
+                {
+                    nums[i]=2;
+                    
+                    two--;
+                }
+        }
+        }
+  
+
+
 };
-/*int n=nums.size();
-        
-       for(int i=0;i<n;i++)
-       {
-           for(int j=i+1;j<n;j++)
-           {
-               if(nums[i]>nums[j])
-               {
-                   int temp=nums[i];
-                   nums[i]=nums[j];
-                   nums[j]=temp;
-               }
-           }
-       }*/     
-        
