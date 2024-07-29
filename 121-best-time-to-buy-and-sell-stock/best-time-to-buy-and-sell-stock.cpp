9ed3ab63c10f90474197998db3ab49bf640f9aac
Kartikay Mehta <71170646+KartikayMehta@users.noreply.github.com>
2024-07-29 18:00:01 +0530

    Time: 77 ms (87.18%) | Memory: 96.2 MB (26.89%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
          int profit = 0;
         int mini = prices[0];
         int cost = 0;
-        for (int i = 0; i< prices.size(); i++) {
+        for (int i = 1; i< prices.size(); i++) {
             cost = prices[i] - mini;
             profit = max(cost, profit);
             mini = min(prices[i], mini);
